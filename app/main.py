bcfa5fee25da5315cf598a1c62694befafbf4b34
Masamichi Tanaka <masa25michi@hotmail.com>
2020-03-01 00:03:44 +0900

    enable app

--- app/main.py
+++ app/main.py
@@ -48,4 +48,4 @@ def handle_message(event):
 app.config.from_object('database.config.Config')
 init_db(app)
 
-# app.run(host="0.0.0.0", port=int(os.getenv("PORT", 5000)))
+app.run(host="0.0.0.0", port=int(os.getenv("PORT", 5000)))
