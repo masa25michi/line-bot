11fd0ca9e253151b80f0d3f2e0dcbc06631ff4ae
Masamichi Tanaka <masa25michi@hotmail.com>
2020-03-07 14:19:05 +0900

    add link to detail page

--- app/bot.py
+++ app/bot.py
@@ -1,5 +1,11 @@
+import requests
+import json
+import pprint
 from models import *
 from app.lib.display_words import get_words_for_display
+from linebot.models import (
+    MessageEvent, PostbackEvent, TextMessage, TextSendMessage, FlexSendMessage
+)
 
 
 def analyze_user_message(user_message):
@@ -8,7 +14,6 @@ def analyze_user_message(user_message):
 
 
 def reply_message(event):
-    print(event)
     response = ''
     user_line_id = event.source.sender_id
     user = User.get_user(user_line_id)
@@ -32,3 +37,79 @@ def reply_message(event):
             response = get_words_for_display(words, chapter)
 
     return response
+
+
+def reply_postback(event):
+    word = event.postback.data
+    params = {'keyword': word}
+    r = requests.get(
+        'https://jisho.org/api/v1/search/words',
+        params=params)
+    response = r.json()
+
+    examples = [
+        {
+            "type": "text",
+            "text": word,
+            "weight": "bold",
+            "size": "xl"
+        },
+        {
+            "type": "separator"
+        }
+    ]
+    count = 1
+    for data in response['data']:
+        example = data['japanese'][0]
+
+        if 'word' in example:
+            text = ' [' + str(count) + ']  ' + example['word'] + \
+                ' (' + example['reading'] + ') \n'
+        else:
+            text = ' [' + str(count) + ']  ' + example['reading'] + ' \n'
+
+        examples.append({
+            "type": "text",
+            "wrap": True,
+            "text": text})
+
+        count += 1
+
+    messages = {
+        "type": "bubble",
+        "body": {
+            "type": "box",
+            "layout": "vertical",
+            "contents": examples
+        },
+        "footer": {
+            "type": "box",
+            "layout": "vertical",
+            "spacing": "sm",
+            "contents": [
+                {
+                    "type": "button",
+                    "style": "link",
+                    "height": "sm",
+                    "action": {
+                        "type": "postback",
+                        "label": "Favorite",
+                        "data": word,
+                        "displayText": word
+                    }
+                },
+                {
+                    "type": "button",
+                    "style": "link",
+                    "height": "sm",
+                    "action": {
+                        "type": "uri",
+                        "label": "More Detail",
+                        "uri": "https://jisho.org/word/" + word
+                    }
+                }
+            ],
+            "flex": 0
+        }
+    }
+    return FlexSendMessage(alt_text=word + 'ってどういう意味？', contents=messages)
--- app/main.py
+++ app/main.py
@@ -1,9 +1,6 @@
 import os
 import atexit
 import app.bot as bot
-import requests
-import json
-import pprint
 # import app.scheduler as line_scheduler
 from database.sqlalchemy import init_db
 # from apscheduler.schedulers.background import BackgroundScheduler
@@ -57,84 +54,7 @@ def handle_message(event):
 
 @handler.add(PostbackEvent)
 def on_postback(event):
-    word = event.postback.data
-    params = {
-        'keyword': word,
-    }
-    r = requests.get(
-        'https://jisho.org/api/v1/search/words',
-        params=params)
-    response = r.json()
-
-    examples = [
-        {
-            "type": "text",
-            "text": word,
-            "weight": "bold",
-            "size": "xl"
-        },
-        {
-            "type": "separator"
-        }
-    ]
-    count = 1
-    for data in response['data']:
-        example = data['japanese'][0]
-
-        if 'word' in example:
-            text = ' [' + str(count) + ']  ' + example['word'] + \
-                ' (' + example['reading'] + ') \n'
-        else:
-            text = ' [' + str(count) + ']  ' + example['reading'] + ' \n'
-
-        examples.append({
-            "type": "text",
-            "wrap": True,
-            "text": text})
-
-        count += 1
-
-    messages = {
-        "type": "bubble",
-        "body": {
-            "type": "box",
-            "layout": "vertical",
-            "contents": examples
-        },
-        "footer": {
-            "type": "box",
-            "layout": "vertical",
-            "spacing": "sm",
-            "contents": [
-                {
-                    "type": "button",
-                    "style": "link",
-                    "height": "sm",
-                    "action": {
-                        "type": "postback",
-                        "label": "❤️ Favorite",
-                        "data": word,
-                        "displayText": word
-                    }
-                },
-                {
-                    "type": "button",
-                    "style": "link",
-                    "height": "sm",
-                    "action": {
-                        "type": "uri",
-                        "label": "More Detail",
-                        "uri": "https://linecorp.com"
-                    }
-                }
-            ],
-            "flex": 0
-        }
-    }
-
-    flex_message = FlexSendMessage(
-        alt_text='Example Here',
-        contents=messages)
+    flex_message = bot.reply_postback(event)
 
     line_bot_api.reply_message(event.reply_token, flex_message)
 
